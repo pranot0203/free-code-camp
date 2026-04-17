0613b042855a56f1c61610a6e8ba590f250df979
pranot solase <166111104+pranot0203@users.noreply.github.com>
2026-04-18 00:07:25 +0530

    Add files via upload

---
+++ python/free code/problem_1.py
@@ -0,0 +1,19 @@
+#In this workshop, you will practice Python data types by building a simple report card printer.
+#As you learned in previous lessons, variables are assigned in this way:
+#greeting = 'Hello World'
+#In the example above, the variable greeting has the value of a string, which is a sequence of characters surrounded by quotes.
+#Create a variable called name and assign it the string Alice. Remember to surround the value with either single or double quotes as shown in the example.
+#Important: Python uses indentation (the spaces at the beginning of a line) to organize code. For this workshop, make sure there are no extra spaces at the start of each line of code. Adding extra spaces will cause an IndentationError and prevent your code from running.
+name = 'Alice'
+
+#You can print the value of a variable using the print() function.
+#Example Code
+#greeting = 'Hello World'
+#print(greeting) # Output: Hello World
+#You will learn more about functions in upcoming lessons. For now, know that a function is a reusable block of code that can be called, or invoked, to run its code, and arguments can be passed to it.
+#In the example above, print(greeting) is a function call, and greeting is the argument of the function.
+#Refer to the example and print the name variable. Check the output in the terminal.
+
+
+
+
